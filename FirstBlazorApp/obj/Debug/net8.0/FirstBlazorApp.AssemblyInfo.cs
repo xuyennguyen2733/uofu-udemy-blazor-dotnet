33b3fa884a8933bbd58f0c216452f273c1a6a4bf
Xuyen Nguyen <xuyennguyen2733@gmail.com>
2026-02-19 13:54:20 -0700

    Implemented reversed string functionality for the Blazor Tasker project

--- FirstBlazorApp/obj/Debug/net8.0/FirstBlazorApp.AssemblyInfo.cs
+++ FirstBlazorApp/obj/Debug/net8.0/FirstBlazorApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FirstBlazorApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+56930c421f2a5a654ee754d7f340181e0cbe0159")]
 [assembly: System.Reflection.AssemblyProductAttribute("FirstBlazorApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FirstBlazorApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
