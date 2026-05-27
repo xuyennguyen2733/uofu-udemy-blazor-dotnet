a1bbbe44e09467c4c47747e88422772d19bc0165
Xuyen Nguyen <xuyennguyen2733@gmail.com>
2026-05-27 14:49:04 -0600

    Create tasker card and implement field validation for task name

---
+++ BlazorTasker/Models/TaskerItem.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorTasker.Models
+{
+    public class TaskerItem
+    {
+        public Guid Id { get; set; }
+
+        [Required(ErrorMessage = "Every task must have a name")]
+        public string Name { get; set; } = "";
+        
+        public bool IsComplete { get; set; } = false;
+    }
+}
